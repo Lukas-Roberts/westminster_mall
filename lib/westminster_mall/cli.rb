2cb7e77cf9f3cb5df8f5425ed70eeb38894899d6
Garth Lilly <garff247@gmail.com>
2019-05-28 21:59:10 +0000

    started methods

--- lib/westminster_mall/cli.rb
+++ lib/westminster_mall/cli.rb
@@ -1,22 +1,21 @@
 class WestminsterMall::CLI
   
   def call
-    puts "The stores of Westminster Mall:"
     list_stores
     directory
     goodbye
   end
   
   def list_stores
-    
+    puts "The stores of Westminster Mall:"
   end
   
   def directory
-    
+    puts "Enter the number of the store you would like to learn more about or type exit:"
   end
   
   def goodbye
-    
+    puts "See you next time!"
   end
   
 end
